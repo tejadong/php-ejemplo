99cd6fc8953a9d000a7756e69e4a7d9e8d97fd2d
Jesús Tejada Pérez <tejadong@gmail.com>
2016-10-11 13:33:37 +0200

    Añadida validación del formulario antes de procesarlo.

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
     session_name('encuesta');
     session_start();
 
+    $error = false;
+
     if (isset($_POST['votar'])) {
         if(!isset($_SESSION["encuestas"])) {
             $_SESSION['encuestas'] = 0;
@@ -12,18 +14,21 @@
             $_SESSION['suma5'] = 0;
         }
 
-        $_SESSION['suma1'] += $_POST["pr1"];
-        $_SESSION['suma2'] += $_POST["pr2"];
-        $_SESSION['suma3'] += $_POST["pr3"];
-        $_SESSION['suma4'] += $_POST["pr4"];
-        $_SESSION['suma5'] += $_POST["pr5"];
+        if (isset($_POST['pr1']) && isset($_POST['pr2']) &&
+            isset($_POST['pr3']) && isset($_POST['pr4']) &&
+            isset($_POST['pr5'])) {
+                $_SESSION['suma1'] += $_POST["pr1"];
+                $_SESSION['suma2'] += $_POST["pr2"];
+                $_SESSION['suma3'] += $_POST["pr3"];
+                $_SESSION['suma4'] += $_POST["pr4"];
+                $_SESSION['suma5'] += $_POST["pr5"];
+        } else {
+            $error = true;
+        }
 
         $_SESSION['encuestas']++;
     }
 
-    echo "<pre>";
-    var_dump($_SESSION);
-    echo "</pre>";
 ?>
 <!doctype html>
 <html lang="es" xmlns="http://www.w3.org/1999/html">
@@ -36,6 +41,11 @@
 </head>
 <body>
     <h1>Enceuesta de HLC</h1>
+    <?php
+        if ($error) {
+            echo '<h1>Cumplimente todas las encuestas, por favor.</h1>';
+        }
+    ?>
     <form method="post" action="index.php">
         <?php
             $valores = [
